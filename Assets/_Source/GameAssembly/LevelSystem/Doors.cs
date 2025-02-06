4d76c869871070627639f729925f83080e244407
red-blues <moyboje@gmail.com>
2025-02-06 22:47:40 +0300

    tweak: update monster settings

--- Assets/_Source/GameAssembly/LevelSystem/Doors.cs
+++ Assets/_Source/GameAssembly/LevelSystem/Doors.cs
@@ -1,103 +1,105 @@
+using DG.Tweening;
 using UnityEngine;
 using UnityEngine.UI;
-using System.Collections;
-using DG.Tweening;
 
-public class DoorTeleporter : MonoBehaviour
+namespace GameAssembly.LevelSystem
 {
-    [Header("Настройки телепортации")]
-    [Tooltip("Объект, который нужно телепортировать (например, игрок)")]
-    [SerializeField] private GameObject objectToTeleport;
-    [SerializeField] private GameObject cameraToTeleport;
-    [Tooltip("Позиция, куда будет телепортирован объект. Может меняться во время игры.")]
-    [SerializeField] private Transform teleportDestination;
-
-    [Header("Настройки взаимодействия")]
-    [Tooltip("Клавиша для активации телепортации")]
-    [SerializeField] private KeyCode teleportKey = KeyCode.E;
-    // [Tooltip("Текст подсказки, который выводится при подходе к двери")]
-    // [SerializeField] private string promptMessage = "Нажмите E чтобы войти";
-    [Tooltip("UI-панель с подсказкой (например, с компонентом Text), которую можно включать/выключать")]
-    [SerializeField] private GameObject promptUI;
-
-    [Header("Настройки затемнения экрана")]
-    [Tooltip("Изображение (например, UI Image), которое покрывает весь экран для эффекта затемнения")]
-    [SerializeField] private Image fadeImage;
-    [Tooltip("Длительность эффекта затемнения/осветления")]
-    [SerializeField] private float fadeDuration = 1f;
-
-    private bool playerInRange = false;
-    private bool isTeleporting = false;
-
-    private void Start()
+    public class DoorTeleporter : MonoBehaviour
     {
-        if(promptUI != null)
-            promptUI.SetActive(false);
+        [Header("Настройки телепортации")]
+        [Tooltip("Объект, который нужно телепортировать (например, игрок)")]
+        [SerializeField] private GameObject objectToTeleport;
+        [SerializeField] private GameObject cameraToTeleport;
+        [Tooltip("Позиция, куда будет телепортирован объект. Может меняться во время игры.")]
+        [SerializeField] private Transform teleportDestination;
 
-        if(fadeImage != null)
-        {
-            Color c = fadeImage.color;
-            c.a = 0;
-            fadeImage.color = c;
-        }
-    }
+        [Header("Настройки взаимодействия")]
+        [Tooltip("Клавиша для активации телепортации")]
+        [SerializeField] private KeyCode teleportKey = KeyCode.E;
+        // [Tooltip("Текст подсказки, который выводится при подходе к двери")]
+        // [SerializeField] private string promptMessage = "Нажмите E чтобы войти";
+        [Tooltip("UI-панель с подсказкой (например, с компонентом Text), которую можно включать/выключать")]
+        [SerializeField] private GameObject promptUI;
 
-    private void Update()
-    {
-        if(playerInRange && !isTeleporting)
+        [Header("Настройки затемнения экрана")]
+        [Tooltip("Изображение (например, UI Image), которое покрывает весь экран для эффекта затемнения")]
+        [SerializeField] private Image fadeImage;
+        [Tooltip("Длительность эффекта затемнения/осветления")]
+        [SerializeField] private float fadeDuration = 1f;
+
+        private bool playerInRange = false;
+        private bool isTeleporting = false;
+
+        private void Start()
         {
-            if(Input.GetKeyDown(teleportKey))
+            if(promptUI != null)
+                promptUI.SetActive(false);
+
+            if(fadeImage != null)
             {
-                TeleportRoutine();
+                Color c = fadeImage.color;
+                c.a = 0;
+                fadeImage.color = c;
             }
         }
-    }
 
-    private void TeleportRoutine()
-    {
-        isTeleporting = true;
-        fadeImage.DOFade(1, fadeDuration)
-        .OnComplete(() =>
-         {
-            fadeImage.DOFade(0, fadeDuration).OnComplete(() =>
+        private void Update()
+        {
+            if(playerInRange && !isTeleporting)
             {
-                isTeleporting = false;
-            });
-            if(objectToTeleport != null && teleportDestination != null)
-            {
-                objectToTeleport.transform.position = teleportDestination.position;
-                cameraToTeleport.transform.position = teleportDestination.position;
+                if(Input.GetKeyDown(teleportKey))
+                {
+                    TeleportRoutine();
+                }
             }
-        });
+        }
+
+        private void TeleportRoutine()
+        {
+            isTeleporting = true;
+            fadeImage.DOFade(1, fadeDuration)
+                .OnComplete(() =>
+                {
+                    fadeImage.DOFade(0, fadeDuration).OnComplete(() =>
+                    {
+                        isTeleporting = false;
+                    });
+                    if(objectToTeleport != null && teleportDestination != null)
+                    {
+                        objectToTeleport.transform.position = teleportDestination.position;
+                        cameraToTeleport.transform.position = teleportDestination.position;
+                    }
+                });
 
         
-    }
+        }
 
-    private void OnTriggerEnter2D(Collider2D collision)
-    {
-        if(collision.gameObject == objectToTeleport)
+        private void OnTriggerEnter2D(Collider2D collision)
         {
-            playerInRange = true;
-            if(promptUI != null)
+            if(collision.gameObject == objectToTeleport)
             {
-                Text textComp = promptUI.GetComponentInChildren<Text>();
-                // if(textComp != null)
-                // {
-                //     textComp.text = promptMessage;
-                // }
-                promptUI.SetActive(true);
+                playerInRange = true;
+                if(promptUI != null)
+                {
+                    Text textComp = promptUI.GetComponentInChildren<Text>();
+                    // if(textComp != null)
+                    // {
+                    //     textComp.text = promptMessage;
+                    // }
+                    promptUI.SetActive(true);
+                }
             }
         }
-    }
 
-    private void OnTriggerExit2D(Collider2D collision)
-    {
-        if(collision.gameObject == objectToTeleport)
+        private void OnTriggerExit2D(Collider2D collision)
         {
-            playerInRange = false;
-            if(promptUI != null)
+            if(collision.gameObject == objectToTeleport)
             {
-                promptUI.SetActive(false);
+                playerInRange = false;
+                if(promptUI != null)
+                {
+                    promptUI.SetActive(false);
+                }
             }
         }
     }
