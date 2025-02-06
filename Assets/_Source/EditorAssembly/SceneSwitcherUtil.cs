157a0d28824fc0ab37d380fdd638c593a0a2acc3
red-blues <moyboje@gmail.com>
2025-02-06 03:21:56 +0300

    feat: deploy scene switch tool util

---
+++ Assets/_Source/EditorAssembly/SceneSwitcherUtil.cs
@@ -0,0 +1,17 @@
+using UnityEditor;
+using UnityEditor.SceneManagement;
+
+namespace EditorAssembly
+{
+    public static class SceneSwitcherUtil
+    {
+        private const string MAIN_SCENE_PATH = "Assets/_Presentation/Scenes/MainScene.unity";
+        private const string SANDBOX_SCENE_PATH = "Assets/_Presentation/Scenes/Sandbox.unity";
+        
+        [MenuItem("Tools/Scenes/Switch To Main Scene")]
+        public static void SwitchToMainScene() => EditorSceneManager.OpenScene(MAIN_SCENE_PATH);
+
+        [MenuItem("Tools/Scenes/Switch To Sandbox Scene")]
+        public static void SwitchToSandboxScene() => EditorSceneManager.OpenScene(SANDBOX_SCENE_PATH);
+    }
+}
