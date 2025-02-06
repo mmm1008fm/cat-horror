fddd8700f9a09f0b1ede07b16abb8a1eac161c8b
mmm1008fm <44507247+mmm1008fm@users.noreply.github.com>
2025-02-07 00:09:03 +0300

    feat - 2nd floor ready

---
+++ Assets/_Source/GameAssembly/LevelSystem/Staircase.cs
@@ -0,0 +1,59 @@
+using DG.Tweening;
+using UnityEngine;
+using UnityEngine.UI;
+using GameAssembly.Utils;
+
+namespace GameAssembly.LevelSystem
+{
+    public class StaircaseTeleporter : MonoBehaviour
+    {
+        [SerializeField] private GameObject objectToTeleport;
+        [SerializeField] private LayerMask playerLayer;
+        [SerializeField] private GameObject cameraToTeleport;
+        [SerializeField] private Transform teleportDestination;
+        [SerializeField] private Collider2D tpCollider;
+        // [Tooltip("Текст подсказки, который выводится при подходе к двери")]
+        // [SerializeField] private string promptMessage = "Нажмите E чтобы войти";
+
+        [SerializeField] private Image fadeImage;
+        [SerializeField] private float fadeDuration = 1f;
+        private bool isTeleporting = false;
+
+        private void Start()
+        {
+            if(fadeImage != null)
+            {
+                Color c = fadeImage.color;
+                c.a = 0;
+                fadeImage.color = c;
+            }
+        }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+      if (LayerMaskUtil.ContainsLayer(playerLayer, other.gameObject.layer))
+      {
+        TeleportRoutine();
+      }
+    }
+        private void TeleportRoutine()
+        {
+            isTeleporting = true;
+            fadeImage.DOFade(1, fadeDuration)
+                .OnComplete(() =>
+                {
+                    fadeImage.DOFade(0, fadeDuration).OnComplete(() =>
+                    {
+                        isTeleporting = false;
+                    });
+                    if(objectToTeleport != null && teleportDestination != null)
+                    {
+                        objectToTeleport.transform.position = teleportDestination.position;
+                        cameraToTeleport.transform.position = teleportDestination.position;
+                    }
+                });
+
+        
+        }
+    }
+}
